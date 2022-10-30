950a89dff64a90eaf0b9e67e02765d972d6074f1
PSC Student <psc@billkuker.com>
2022-10-30 14:09:14 -0400

    Implemented addWord and search functions.

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,21 @@
-//Fill in this empty file
+let words = [];
+
+export function search(word){
+    for(let i = 0; i < words.length; i++){
+        if(word == words[i]){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export function addWord(word){
+    words.push(word);
+
+}
+
+addWord("aardvark");
+addWord("fermenting");
+addWord("photograph");
+addWord("zygotes");
